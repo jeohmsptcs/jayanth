9ce547a57de3af44c66515e03d512326512f9e3e
jeohmsptcs <137598837+jeohmsptcs@users.noreply.github.com>
2023-06-24 12:39:08 +0530

    Add files via upload

---
+++ awz.py
@@ -0,0 +1,129 @@
+import time
+#function defination
+def insertionsort(arr):
+    for i in range(1,len(arr)):
+        key=arr[i]
+        j=i-1
+        while j>=0 and key<arr[j]:
+          arr[j+1]=arr[j]
+          j=j-1
+#place key at after the element just smaller than it
+          arr[j+1]=key
+#driver code
+arr=[9,8,7,6,5,4]
+start=time.time()
+print("the orginial array is:")
+for i in range(len(arr)):
+    print(arr[i])
+#function call
+insertionsort(arr)
+import time
+#function defination
+def insertionsort(arr):
+    for i in range(1,len(arr)):
+        key=arr[i]
+        j=i-1
+        while j>=0 and key<arr[j]:
+          arr[j+1]=arr[j]
+          j=j-1
+#place key at after the element just smaller than it
+          arr[j+1]=key
+#driver code
+arr=[9,8,7,6,5,4]
+start=time.time()
+print("the orginial array is:")
+for i in range(len(arr)):
+    print(arr[i])
+#function call
+import time
+#function defination
+def insertionsort(arr):
+    for i in range(1,len(arr)):
+        key=arr[i]
+        j=i-1
+        while j>=0 and key<arr[j]:
+          arr[j+1]=arr[j]
+          j=j-1
+#place key at after the element just smaller than it
+          arr[j+1]=key
+#driver code
+arr=[9,8,7,6,5,4]
+start=time.time()
+print("the orginial array is:")
+for i in range(len(arr)):
+    print(arr[i])
+#function call
+insertionsort(arr)
+import time
+#function defination
+def insertionsort(arr):
+    for i in range(1,len(arr)):
+        key=arr[i]
+        j=i-1
+        while j>=0 and key<arr[j]:
+          arr[j+1]=arr[j]
+          j=j-1
+#place key at after the element just smaller than it
+          arr[j+1]=key
+#driver code
+arr=[9,8,7,6,5,4]
+start=time.time()
+print("the orginial array is:")
+for i in range(len(arr)):
+    print(arr[i])
+#function call
+insertionsort(arr)
+print("the sorted array is:")
+for i in range(len(arr)):
+    print(arr[i])
+end=time.time()
+import time
+#function defination
+def insertionsort(arr):
+    for i in range(1,len(arr)):
+        key=arr[i]
+        j=i-1
+        while j>=0 and key<arr[j]:
+          arr[j+1]=arr[j]
+          j=j-1
+#place key at after the element just smaller than it
+          arr[j+1]=key
+#driver code
+arr=[9,8,7,6,5,4]
+start=time.time()
+print("the orginial array is:")
+for i in range(len(arr)):
+    print(arr[i])
+#function call
+insertionsort(arr)
+print("the sorted array is:")
+for i in range(len(arr)):
+    print(arr[i])
+end=time.time()
+import time
+#function defination
+def insertionsort(arr):
+    for i in range(1,len(arr)):
+        key=arr[i]
+        j=i-1
+        while j>=0 and key<arr[j]:
+          arr[j+1]=arr[j]
+          j=j-1
+#place key at after the element just smaller than it
+          arr[j+1]=key
+#driver code
+arr=[9,8,7,6,5,4]
+start=time.time()
+print("the orginial array is:")
+for i in range(len(arr)):
+    print(arr[i])
+#function call
+insertionsort(arr)
+print("the sorted array is:")
+for i in range(len(arr)):
+    print(arr[i])
+end=time.time()
+print("runtime of the program is:",end-start)
+
+
+
